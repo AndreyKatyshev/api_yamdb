1797074aa0e26406e059d2817a944e70b02e0a3e
Андрей Катышев <Andrey-Katyshev@mail.ru>
2022-07-20 14:29:57 +0300

    13 failed 40 passed

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('api/', include('users.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path(
@@ -10,5 +11,4 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    path('api/', include('users.urls'))
 ]
