8dcfcf099dfbdc2eef970c05acaa00aed658c92d
Никита Боровиков <rikkimari@gmail.com>
2022-07-24 19:31:37 +0600

    git push github master

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,7 +15,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -26,6 +25,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'django_filters',
     'reviews',
+    'users.apps.UsersConfig',
     'api',
 ]
 
